6c29e6adfa459c608259fe8a9eece4224157cefe
tfaaguilar <e_aguilarvelez@fanshaweonline.ca>
2023-01-25 20:36:26 -0500

    adjusting js
    adjust js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,27 @@
 console.log('Javascript is running');
 
-let theanchor = document.querySelector("#newanchor");
-let vectorBag = document.querySelector("#vectorBag");
-let vectorCoctail = document.querySelector("#vectorCoctail");
-let vectorHotel = document.querySelector("#vectorHotel");
-let vectorIsland = document.querySelector("#vectorIsland");
-let vectorLifesaver = document.querySelector("#vectorLifesaver");
-let vectorMap = document.querySelector("#vectorMap");
-let vectorMountain = document.querySelector("#vectorMountain");
-let vectorPlane = document.querySelector("#vectorPlane");
-let vectorSandals = document.querySelector("#vectorSandals");
-let vectorShip = document.querySelector("#vectorShip");
-let vectorTent = document.querySelector("#vectorTent");
+let vectoranchor = document.querySelector("#anchor");
+let vectorbag = document.querySelector("#bag");
+let vectormap = document.querySelector("#map");
+let vectorplane = document.querySelector("#plane");
+let vectorlifesaver = document.querySelector("#lsaver");
+let vectortent = document.querySelector("#tent");
+let vectorsandals = document.querySelector("#sandals");
+let vectorisland = document.querySelector("#island");
+let vectormountain = document.querySelector("#mountain");
+let vectorship = document.querySelector("#ship");
 
 function logID() {
     console.log('user clicked on this graphic', this.id);
 }
 
-theanchor.addEventListener('click', logID);
-vectorBag.addEventListener('click', logID);
-vectorCoctail.addEventListener('click', logID);
-vectorHotel.addEventListener('click', logID);
-vectorIsland.addEventListener('click', logID);
-vectorLifesaver.addEventListener('click', logID);
-vectorMap.addEventListener('click', logID);
-vectorMountain.addEventListener('click', logID);
-vectorPlane.addEventListener('click', logID);
-vectorSandals.addEventListener('click', logID);
-vectorShip.addEventListener('click', logID);
-vectorTent.addEventListener('click', logID);
+vectoranchor.addEventListener('click', logID);
+vectorbag.addEventListener('click', logID);
+vectorisland.addEventListener('click', logID);
+vectorlifesaver.addEventListener('click', logID);
+vectormap.addEventListener('click', logID);
+vectormountain.addEventListener('click', logID);
+vectorplane.addEventListener('click', logID);
+vectorsandals.addEventListener('click', logID);
+vectorship.addEventListener('click', logID);
+vectortent.addEventListener('click', logID);
